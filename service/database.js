3a94586783f7affc6883e1b0ac21886d8222e2f8
tylerbarney <willardbarney@gmail.com>
2024-11-23 17:42:45 -0700

    Added database connection

--- service/database.js
+++ service/database.js
@@ -1,12 +1,19 @@
-const { MongoClient } = require('mongodb')
-const config = require('./dbConfig.json')
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}/?retryWrites=true&w=majority&appName=Cluster0`
-const client = new MongoClient(url)
-const db = client.db('rental')
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('simon');
+const userCollection = db.collection('user');
+const scoreCollection = db.collection('score');
+
+// This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
-    await client.connect();
-    await db.command({ ping: 1 });
-  })().catch((ex) => {
-    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-    process.exit(1);
-  });
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
